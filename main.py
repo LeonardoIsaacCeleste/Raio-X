88b94fe1e3223c589c7848d907c4b5d12ec5bb02
LeonardoIsaacCeleste <130566419+LeonardoIsaacCeleste@users.noreply.github.com>
2023-04-26 21:51:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import serial
+from selenium import webdriver
+from time import sleep
+import os
+
+caminho = "file:"+os.path.dirname(os.path.abspath(__file__))+ "/Site/testeSiteHtml.html"
+
+porta = 'COM5'
+
+ser = serial.Serial(porta, 9600)
+
+maxRange = ser.read_until().decode('ascii').strip()
+
+a = True
+while a:
+    try:
+        maxRange = ser.read_until().decode('ascii').strip() #Maximo
+        print("Max "+maxRange)
+    except:
+        a=False
+driver = webdriver.Chrome()
+
+driver.get(caminho)
+
+dataAnterior = ser.read_until().decode('ascii').strip()
+
+while True:
+    data = ser.readline().decode().strip()  # leitura da porta serial
+    print(f"Valor do encoder: {data}")  # exibição do valor do encode
+    if(dataAnterior != data):
+        driver.execute_script(f"moverHorizontalmente("+str(data)+","+str(maxRange)+");")
+        dataAnterior = data
